d496023ac5b9b18e944b46d4d12a616568384e3b
B.Beik <beik.behrouzbeik>
2022-11-07 17:01:15 +0330

    package page

--- Home/views.py
+++ Home/views.py
@@ -4,19 +4,19 @@ from .models import MyCompanyInfo,City,Tour
 # Create your views here.
 
 def index(request):
-    myCompanyInfo = MyCompanyInfo.objects.get(company_name='Behrouz Travel')
+    # myCompanyInfo = MyCompanyInfo.objects.get(company_name='Behrouz Travel')
     cities = City.objects.filter(home_page_display=True).order_by('-id')[:6]
     destinations = City.objects.all()
     tour = Tour.objects.filter(home_page_display=True).order_by('-id')[:6]
     context = {
-        'title': myCompanyInfo.company_name,
-        'email': myCompanyInfo.email,
-        'phone': myCompanyInfo.phon_number,
-        'facebook': myCompanyInfo.facebook,
-        'twiter': myCompanyInfo.twiter,
-        'linkedin': myCompanyInfo.linkedin,
-        'insta': myCompanyInfo.insta,
-        'utube': myCompanyInfo.utube,
+        # 'title': myCompanyInfo.company_name,
+        # 'email': myCompanyInfo.email,
+        # 'phone': myCompanyInfo.phon_number,
+        # 'facebook': myCompanyInfo.facebook,
+        # 'twiter': myCompanyInfo.twiter,
+        # 'linkedin': myCompanyInfo.linkedin,
+        # 'insta': myCompanyInfo.insta,
+        # 'utube': myCompanyInfo.utube,
         'destinations': destinations,
         'SignIn': 'Sign In',
         'SignOut': 'SignOut',
@@ -36,4 +36,23 @@ def services(request):
     return (render(request,'Home/service.html'))
 
 def package(request):
-    return (render(request,'Home/package.html'))
+    tour = Tour.objects.all().order_by('-id')
+    context = {
+        # 'title': myCompanyInfo.company_name,
+        # 'email': myCompanyInfo.email,
+        # 'phone': myCompanyInfo.phon_number,
+        # 'facebook': myCompanyInfo.facebook,
+        # 'twiter': myCompanyInfo.twiter,
+        # 'linkedin': myCompanyInfo.linkedin,
+        # 'insta': myCompanyInfo.insta,
+        # 'utube': myCompanyInfo.utube,
+        # 'destinations': destinations,
+        'SignIn': 'Sign In',
+        'SignOut': 'SignOut',
+        'SignUp': 'SignUp',
+        'Search': 'Search',
+        'Services': 'Services',
+        'Menu': 'Menu',
+        'tour':tour,
+    }
+    return (render(request,'Home/package.html', context))
